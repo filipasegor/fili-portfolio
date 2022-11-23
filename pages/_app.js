6975ce22ef1708c8a1e34bc8873a0d7e77afe107
Egor Filipas <filipasegor@gmail.com>
2022-11-23 15:41:28 +0300

    Final Deploy vol.1.3.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.scss';
 import Layout from "./components/Layout"
+import usePreserveScroll from "./components/usePreserveScroll.tsx"
+import { useRouter } from "next/router"
+import { useEffect, useRef } from "react"
 
 export default function App({ Component, pageProps }) {
+  usePreserveScroll()
   return (
   <>
     <Layout>
