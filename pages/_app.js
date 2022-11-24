19b704cadd643a705505f3d48ce713b933c664fd
Egor Filipas <filipasegor@gmail.com>
2022-11-24 15:04:30 +0300

    Final Deploy vol.1.3.3

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/globals.scss';
 import Layout from "./components/Layout"
 import { useRouter } from "next/router"
 import { useEffect, useRef } from "react"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
   <>
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   </>
 )
