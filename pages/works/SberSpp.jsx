aa9470dbe8a200382547610ddc696ca60241d5a5
Egor Filipas <filipasegor@gmail.com>
2023-03-15 23:36:57 +0300

    vol 1.6.2

--- pages/works/SberSpp.jsx
+++ pages/works/SberSpp.jsx
@@ -137,6 +137,12 @@ export default function Project(props){
           </DescriptionLayout>
 
           <Media
+            label="false"
+            media="video"
+            src={require('../../public/works/sberspp/building.mp4')}
+          />
+
+          <Media
             srcThumb={thumb_ds} 
             altThumb="thumb_ds"
             label="false"
