f0e0dbae49bd7051a16aa5304e7b2649945046f2
Egor Filipas <filipasegor@gmail.com>
2023-10-04 18:08:50 +0300

    vol.20.0.0

--- pages/components/works/Media.jsx
+++ pages/components/works/Media.jsx
@@ -1,23 +1,21 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 
-import styles from '../../../styles/works/Media.module.scss'
+import styles from "../../../styles/works/Media.module.scss";
 
+export default function Media(props) {
+  const [loaded, setLoaded] = useState(false);
 
-
-export default function Media(props){
-
-  const [loading, setLoading] = useState(false);
-
-  function loadingData(){
-    setLoading(true);
+  function loadedData() {
+    setLoaded(true);
+    console.log("Video loaded!");
   }
 
   // const video = document.getElementById('video');
   // const videoWorks = !!document.createElement('video').canPlayType;
-  
+
   // function removeControls(){
   //   if (videoWorks) {
   //     video.controls = false;
@@ -25,61 +23,61 @@ export default function Media(props){
   //   }
   // }
 
-
   const img = (
-      <div className={styles.imgWraper}>
-        <Image
-         className={styles.Image}
-         alt="media-img"
-         src={props.src}
-         layout="responsive"
-         objectFit="cover"
-         placeholder="blur"
-       />
-      </div>
+    <div className={styles.imgWraper}>
+      <Image
+        className={styles.Image}
+        alt="media-img"
+        src={props.src}
+        layout="responsive"
+        objectFit="cover"
+        placeholder="blur"
+      />
+    </div>
   );
 
   const video = (
     <div className={styles.videoWrapper}>
       <div
-      className={styles.thumbWrapper}
-      style={{ display: loading ? "none" : "block" }}>
+        className={styles.thumbWrapper}
+        style={{ display: loaded ? "none" : "block" }}>
         <Image
-         className={styles.thumb}
-         alt={props.altThumb}
-         src={props.srcThumb}
-         layout="responsive"
-         objectFit="cover"
-       />
+          className={styles.thumb}
+          alt={props.altThumb}
+          src={props.srcThumb}
+          layout="responsive"
+          objectFit="cover"
+        />
       </div>
-      <video id="video" autoPlay muted loop playsInline
-      className={styles.video}
-      onLoadedData={() => {
-          loadingData();
+      <video
+        id="video"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className={styles.video}
+        onLoadedData={() => {
+          loadedData();
         }}
-      style={{ display: loading ? "block" : "none" }}>
-      <source src={props.src} />
+        style={{ display: loaded ? "block" : "none" }}>
+        <source src={props.src} />
       </video>
     </div>
   );
 
-
   const label = (
-    <div className={styles.labelWrapper}
-    style ={{justifyContent: props.left ? "flex-start" : "flex-end"}}
-    >
-      <div className={styles.textWrapper}>
-       {props.children}
-      </div>
+    <div
+      className={styles.labelWrapper}
+      style={{ justifyContent: props.left ? "flex-start" : "flex-end" }}>
+      <div className={styles.textWrapper}>{props.children}</div>
     </div>
-  )
-  
-  return(
+  );
+
+  return (
     <>
       <div
-      className={styles.mediaWrapper}
-      style={{marginTop: props.label === "true" ? "60px" : "0px"}}
-      >
+        className={styles.mediaWrapper}
+        style={{ marginTop: props.label === "true" ? "60px" : "0px" }}>
         {props.label === "true" && label}
         {props.media === "video" ? video : img}
       </div>
--- pages/components/works/Video.jsx
+++ pages/components/works/Video.jsx
@@ -1,22 +1,20 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-import {useState, useEffect, useRef} from "react"
+import { useState, useEffect, useRef } from "react";
 
-import styles from '../../../styles/works/Video.module.scss'
+import styles from "../../../styles/works/Video.module.scss";
 
+export default function Video(props) {
+  const [loaded, setLoaded] = useState(false);
 
-
-export default function Video(props){
-
-  const [loading, setLoading] = useState(false);
   const [currentVideoTime, setCurrentVideoTime] = useState(0);
   const [startstop, setStartstop] = useState(true);
   const [videoDuration, setVideoDuration] = useState(0);
- 
 
-  function loadingData(){
-    setLoading(true);
+  function loadedData() {
+    setLoaded(true);
+    console.log("Video loaded!");
   }
 
   const ref = useRef(null);
@@ -27,65 +25,75 @@ export default function Video(props){
     video.currentTime = currentVideoTime;
     setVideoDuration(Math.floor(video.duration));
 
-    if(startstop){
+    if (startstop) {
       setCurrentVideoTime(Math.floor(video.currentTime));
 
       const intervalId = setInterval(() => {
         setCurrentVideoTime(video.currentTime + 1);
-      }, 1000)
+      }, 1000);
 
       return () => {
-        clearInterval(intervalId)
-      }    
-
-      }
-  
+        clearInterval(intervalId);
+      };
+    }
   });
 
-
-  function handleProgressChange(e){
-    setCurrentVideoTime(Number(e.target.value))
+  function handleProgressChange(e) {
+    setCurrentVideoTime(Number(e.target.value));
   }
 
   function handleStopClick() {
     const video = ref.current;
     video.pause();
-    setStartstop(false)
+    setStartstop(false);
   }
 
   function handleStartClick() {
     const video = ref.current;
     video.play();
-    setStartstop(true)
+    setStartstop(true);
   }
 
   const label = (
-    <div className={styles.labelWrapper}
-    style ={{justifyContent: props.left ? "flex-start" : "flex-end"}}
-    >
-      <div className={styles.textWrapper}>
-       {props.children}
-      </div>
+    <div
+      className={styles.labelWrapper}
+      style={{ justifyContent: props.left ? "flex-start" : "flex-end" }}>
+      <div className={styles.textWrapper}>{props.children}</div>
     </div>
   );
 
   const stop = (
-    <button className={styles.stopButton} onClick={handleStopClick}></button>
+    <button className={styles.playStopButton} onClick={handleStopClick}>
+      <svg className={styles.svg}>
+        <path
+          transform="translate(-10,-3)"
+          d="M23.9,38.5h-2.3c-1.3,0-2.3-1-2.3-2.2V19.7c0-1.3,1.1-2.3,2.3-2.2h2.3c1.3,0,2.3,1,2.3,2.2v16.5 C26.2,37.5,25.2,38.5,23.9,38.5 M34.4,38.5c1.3,0,2.3-1,2.3-2.2V19.7c0-1.3-1.1-2.3-2.3-2.2h-2.3c-1.3,0-2.3,1-2.3,2.2v16.5 c0,1.3,1.1,2.3,2.3,2.2H34.4z"></path>
+      </svg>
+    </button>
   );
 
   const start = (
-    <button className={styles.startButton} onClick={handleStartClick}></button>
+    // <button className={styles.startButton} onClick={handleStartClick}></button>
+    <button className={styles.playStopButton} onClick={handleStartClick}>
+      <svg className={styles.svg}>
+        <path
+          transform="translate(-10, -3)"
+          d="M20.8,36V20c0-1.6,1-2.5,2.3-2.5c0.7,0,1.1,0.1,1.7,0.5l13.4,7.7c1.2,0.7,1.8,1.2,1.8,2.3 c0,1.1-0.6,1.6-1.8,2.3L24.8,38c-0.6,0.4-1,0.5-1.7,0.5C21.8,38.5,20.8,37.6,20.8,36"></path>
+      </svg>
+    </button>
   );
-  
-  return(
+
+  return (
     <>
-      <div className={styles.mediaWrapper} style={{marginTop: props.label === "true" ? "60px" : "0px"}} >
+      <div
+        className={styles.mediaWrapper}
+        style={{ marginTop: props.label === "true" ? "60px" : "0px" }}>
         {props.label === "true" && label}
         <div className={styles.videoWrapper}>
           <div
             className={styles.thumbWrapper}
-            style={{ display: loading ? "none" : "block" }}>
-              <Image
+            style={{ display: loaded ? "none" : "block" }}>
+            <Image
               className={styles.thumb}
               alt={props.altThumb}
               src={props.srcThumb}
@@ -93,31 +101,37 @@ export default function Video(props){
               objectFit="cover"
             />
           </div>
-          <div style={{ display: loading ? "block" : "none" }}>
-          <video 
-            onLoadedData={() => {
-              loadingData();
-            }}
-            ref={ref}
-            autoPlay muted loop playsInline controls 
-            className={styles.video}>
-            <source src={props.src} />
-          </video>
+          <div style={{ display: loaded ? "block" : "none" }}>
+            <video
+              onCanPlayThrough={() => {
+                loadedData();
+              }}
+              ref={ref}
+              autoPlay
+              muted
+              loop
+              playsInline
+              controls
+              className={styles.video}>
+              <source src={props.src} />
+            </video>
             <div className={styles.currentTimeWrapper}>
               <div className={styles.ButtonWrapper}>
                 {startstop ? stop : start}
               </div>
               <div className={styles.timeLineWrapper}>
                 <span className={styles.currentTimeWrapperText}>
-                  {currentVideoTime < 10 ? `0:0${currentVideoTime}` : `0:${currentVideoTime}`}
+                  {currentVideoTime < 10
+                    ? `0:0${currentVideoTime}`
+                    : `0:${currentVideoTime}`}
                 </span>
                 <input
-                className={styles.progressBarInput}
-                type="range"
-                min={props.min}
-                max={props.max}
-                value={currentVideoTime}
-                onChange={(e) => handleProgressChange(e)}
+                  className={styles.progressBarInput}
+                  type="range"
+                  min={props.min}
+                  max={props.max}
+                  value={currentVideoTime}
+                  onChange={(e) => handleProgressChange(e)}
                 />
                 <span className={styles.currentTimeWrapperText}>
                   {`0:${videoDuration}`}
